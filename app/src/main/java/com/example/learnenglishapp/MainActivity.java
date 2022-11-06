7dd821a11a5b26220d7b3c3dd0217d3c8d73aac1
tan <nst20011210@gmail.com>
2022-11-07 01:56:42 +0700

    Đăng nhập đăng ký từ email và mật khẩu từ Firebase. Đặt lại mật khẩu bằng link gửi về email. Đổi icon app

--- app/src/main/java/com/example/learnenglishapp/MainActivity.java
+++ app/src/main/java/com/example/learnenglishapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.learnenglishapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,23 +15,22 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private Toolbar mainToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        hamKhaiBao();
-        hamDieuKhien();
-    }
-
-    private void hamKhaiBao() {
         firebaseAuth = FirebaseAuth.getInstance();
+        mainToolbar = findViewById(R.id.main_toolbar);
+        getSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle("Learn English");
     }
 
-    private void hamDieuKhien() {
+    private void getSupportActionBar(Toolbar mainToolbar) {
     }
-
 
     protected void onStart() {
 
@@ -38,5 +41,24 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, SignUpActivity.class));
             finish();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.profile_menu){
+            
+        } else {
+            firebaseAuth.signOut();
+            startActivity(new Intent(MainActivity.this , LogInActivity.class));
+            finish();
+        }
+
+        return true;
     }
 }
